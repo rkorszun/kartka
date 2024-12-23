2fcdd6bf25ba74937844a840c3a90d8072a845fa
Rafal Korszun <rafal.korszun.extern@regiondo.com>
2024-12-23 17:51:33 +0100

    refactor build structure; remove build directory from .gitignore and add necessary build files

---
+++ build/precache-manifest.a3d35e6decc12c8c380767fa9461e8e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62853666d47c84486fc80151b821a05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3ef8b9548553fd6a01c",
+    "url": "/static/css/main.e77ccfbf.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "c3ef8b9548553fd6a01c",
+    "url": "/static/js/main.044845bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d73af02be54d57ba1fd4bf4587be3cce",
+    "url": "/static/media/background.d73af02b.jpg"
+  }
+]);
